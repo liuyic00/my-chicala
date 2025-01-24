20f209d5321dd78022305ce0525b34361d6b0c75
liuyic00 <liuyic00@gmail.com>
2025-01-24 13:51:48 +0800

    test(case): add sv2chisel helper vecconvert cases

---
+++ testcase/src/main/scala/sv2chisel/vecconvert.scala
@@ -0,0 +1,17 @@
+package sv2chisel
+
+import chisel3._
+import chisel3.util._
+import sv2chisel.helpers.vecconvert._
+
+class VecConvert(width1: Int, width2: Int) extends Module {
+
+  val a = IO(Input(Bool()))
+  val c = IO(Output(Vec(width1 + 1, Bool())))
+
+  c(width1, 0) := (
+    VecInit.tabulate(width1 + 1)(_ => a).asUInt
+      & Fill(width1 + 1, true.B)
+  ).asTypeOf(Vec(width1 + 1, Bool()))
+
+}
